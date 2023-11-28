16f89e52ed3a3c4308abbb25fbec05910898731e
Ibrakadabr4 <151637469+Ibrakadabr4@users.noreply.github.com>
2023-11-28 20:06:36 +0100

    Lab09es3.C

--- Lab09es3.C
+++ Lab09es3.C
@@ -15,14 +15,31 @@ sfera* caricaInteriFile(char nomefile[], int& quanti){
     char b;
     ifstream file_in;
     file_in.open(nomefile);
-    file_in>>a;
-    while(!file_in.eof()){
-        file_in>>a >> b;
-        quanti++;
+    if(file_in.fail()){
+        quanti = -1;
+        return NULL;
     }
-    file = new sfera[quanti];
+    
+    file_in>>a >>b;
+    while(!file_in.eof()){
+        quanti++;
+        file_in>>a >> b;
+
+    }
     file_in.close();
+
+    file = new sfera[quanti];
+    if(!file){
+        quanti = -2;
+        return NULL;
+    }
+
     file_in.open(nomefile);
+    if(file_in.fail()){
+        quanti = -3;
+        return NULL;
+    }
+    
     for(int i=0; i<quanti; i++){
         file_in>> file[i].diam >> file[i].colore;
     }
@@ -32,18 +49,24 @@ sfera* caricaInteriFile(char nomefile[], int& quanti){
 
 
     return file;
-}
+};
 
 int main(){
     int count = 0;
     sfera* v = NULL;
 
 
-    char nome[] = "interi.dat";
+    char nome[] = "sfere.dat";
     v = caricaInteriFile(nome, count);
 
-    //stampa dati
+    //stampa dati: numero dati, i primi e ultimi tre
     cout << endl<<"Numero dati: "<<count;
+    for(int i=0; i<3; i++){
+        cout<<endl<< v[i].diam <<v[i].colore;
+    }
+    for(int i=(count - 3); i<count; i++){
+        cout<<endl<< v[i].diam <<v[i].colore;
+    }
     
     delete []v;
     v = NULL;
